819ff778bf29118e6fbbd7145b55c88d94855cc4
John Kabler <john.kabler@gmail.com>
2014-04-23 11:39:13 -0400

    Added in pipeline and cross validation

--- initial_classification_ML.py
+++ initial_classification_ML.py
@@ -45,3 +45,47 @@ print(predictions)
 #Initial success, but needs work.  Still uses bag of words,
 #which won't stand up to real complexity.  Possibly need to
 #incorporate sentiment analysis.
+
+#Let's use pipeline to clean up the testing process
+
+from sklearn.pipeline import Pipeline
+
+#Bernoulli is often more appropriate for tweets and other short messages
+from sklearn.naive_bayes import BernoulliNB
+
+
+pipeline = Pipeline([
+                     ('vectorizer', StemmedCountVectorizer(min_df=1, stop_words='english')),
+#                      ('tfidf_transformer',  TfidfTransformer()),
+                     ('classifier', BernoulliNB(binarize=0.0))
+                     ])
+
+
+pipeline.fit(np.asarray(tweets_party.tweet), targets)
+
+
+pipeline.predict(examples)
+
+
+# ## Cross Validation Time
+
+from sklearn.cross_validation import KFold
+
+
+k_fold = KFold(n=len(tweets_party), n_folds=6, indices=False)
+scores = []
+
+
+for train_indices, test_indices in k_fold:
+    train_text = np.asarray(tweets_party[train_indices]['tweet'])
+    train_y = np.asarray(tweets_party[train_indices]['party_affiliation'], dtype="|S6")
+
+    test_text = np.asarray(tweets_party[test_indices]['tweet'])
+    test_y = np.asarray(tweets_party[test_indices]['party_affiliation'], dtype="|S6")
+
+    pipeline.fit(train_text, train_y)
+    score = pipeline.score(test_text, test_y)
+    scores.append(score)
+    
+score = sum(scores) / len(scores)
+print(score)
